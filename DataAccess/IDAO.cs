79ba75ab68a50282db28ec91aa5f6ecff3a4a2c0
Nnorak <leonard.morris@gmail.com>
2023-06-16 17:12:15 -0400

    clean up IDAO interface

--- DataAccess/IDAO.cs
+++ DataAccess/IDAO.cs
@@ -32,61 +32,10 @@ namespace Assailant.DataAccess
 
     public interface IDAO
     {
-        /* I do not completely understand the purpose and differences of
-         * GetConnection/GetTransaction/SetConnection/SetTransaction
-         * so I do not know what to make them do.
-         * I also do really know the difference of DbConnection and
-         * MySqlConnection since I have never used the former. */
-
-        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
-        *    DbConnection GetConnection();                                                       *
-        *    DbTransaction GetTransaction();                                                     *
-        *    void SetConnection(DbConnection conn, DbTransaction transaction = null);            *
-        *    void SetTransaction(DbTransaction transaction = null);                              *
-        * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-        /* Okay now I think I understand a little more. 
-         * I am assuming (as the name suggests) "SetConnection" is creating the connection.
-         * GetConnection returns the created connection.
-         * My function "CreateDBConnection" does both. */
-
-        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-        *    MySqlConnection CreateDBConnection()                                *
-        *    {                                                                   *
-        *        MySqlConnection connection = new MySqlConnection(conStr);       *
-        *        connection.Open();                                              *
-        *        return connection;                                              *
-        *    }                                                                   *
-        * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-        /* On second thought I might not understand it.
-         * I just made what I thought set connection should be: */
-
-        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-        *    void SetConnection(string conn, MySqlTransaction transaction = null)  *
-        *    {                                                                     *
-        *        MySqlConnection connection = new MySqlConnection(conn);           *
-        *    }                                                                     *
-        * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-        /* ...but realised that what you put in the arugements was "DbConnection conn"
-         * meaning that since it requires a DbConnection (vs mine which required a string
-         * to make the connection) you already made a connection. 
-         * So now I'm thinking that GetConnection is more similar to my CreateDBConection
-         * and I have no idea what the purpose of SetConnection is.*/
-
-        // This is what I have come up with so far:
-        MySqlConnection GetConnection()
-        {
-            MySqlConnection connection = new MySqlConnection("database=assailantdb;server=fserver2;user=jtmorris;pwd=Lmjabc!23");
-            connection.Open();
-            return connection;
-        }
+        DbConnection GetConnection();
         DbTransaction GetTransaction();
         void SetConnection(DbConnection conn, DbTransaction transaction = null);
         void SetTransaction(DbTransaction transaction = null);
-
-        // Don't know if this is right.
-        // I also don't fully understand what instances are so I what instances are so I have some learning to do.
     }
+}
 }
